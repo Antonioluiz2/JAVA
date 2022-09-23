43b80c3265f0c9b1a3dbdbda28d57946e73cfb37
Antonioluiz2 <antonioluizti@hotmail.com>
2022-09-23 15:03:26 -0300

    test

---
+++ aula.01/aula/escola.java
@@ -0,0 +1,11 @@
+package  aula;
+
+    public class escola{
+        public static void main(String[] args) {
+            System.out.println("Olá Mundo");
+            System.out.println(2*5);
+
+            }
+    }
+   
+
---
+++ index.java
@@ -0,0 +1,9 @@
+/**
+ * index
+ */
+public class index {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
